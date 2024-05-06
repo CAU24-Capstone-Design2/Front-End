a0c0f6d4aa2c93ce720cc1b846688d867411b0f0
Yoo Han <hanyoojin39@gmail.com>
2024-05-06 23:35:28 +0900

    feat: create container for image tilt

--- lib/widget/widget_mytattoTilt.dart
+++ lib/widget/widget_mytattoTilt.dart
@@ -12,7 +12,19 @@ class _MytattoTiltState extends State<MytattoTilt> {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      alignment: Alignment.center,
+      margin: const EdgeInsets.only(left: 60),
+      child: Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateY(-1.0),
+        child: Stack(
+          children: [
+            Image.asset('assets/airplane.jpg'),
+            Image.asset('assets/nobg_masked_airplane.png'),
+          ],
+        ),
+      ),
     );
   }
 }
