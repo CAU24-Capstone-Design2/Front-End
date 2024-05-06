de1d13865ca9264480901f9d4dec8dd1d63f0817
Yoo Han <hanyoojin39@gmail.com>
2024-05-06 23:32:51 +0900

    feat: create mytattoTilt widget

---
+++ lib/widget/widget_mytattoTilt.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MytattoTilt extends StatefulWidget {
+  const MytattoTilt({Key? key}) : super(key: key);
+
+  @override
+  _MytattoTiltState createState() => _MytattoTiltState();
+}
+
+class _MytattoTiltState extends State<MytattoTilt> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
