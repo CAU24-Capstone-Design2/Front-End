76f8c49c130c250f722a62bb52a6eeef34c7586d
Yoo Han <hanyoojin39@gmail.com>
2024-05-11 22:02:05 +0900

    feat: add request for myinfo screen

--- lib/screen/screen_myinfo.dart
+++ lib/screen/screen_myinfo.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -5,7 +6,11 @@ import 'package:provider/provider.dart';
 import 'package:scart/util/kakaoController.dart';
 import 'package:scart/widget/widget_customAppBar.dart';
 import 'package:scart/widget/widget_customNavBar.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../util/AllTattooList.dart';
+import '../util/Tattoo.dart';
 import '../widget/widget_customhomeFAB.dart';
 
 class MyinfoScreen extends StatefulWidget {
@@ -16,6 +21,99 @@ class MyinfoScreen extends StatefulWidget {
 }
 
 class MyinfoState extends State<MyinfoScreen> {
+  final storage = FlutterSecureStorage();
+  var scarId = 0;
+  var isFirst = true;
+  Future<List<AllTattooList>>? futureAllTattoo;
+  late Future<Tattoo> futureTattoo;
+
+  Future<bool> checkIsFirstUser() async {
+    final url = Uri.http('165.194.104.144:8888', '/api/user/checkIsFirstUser');
+
+    try {
+      var appToken = await storage.read(key: 'appToken');
+
+      final response = await http.get(url, headers: {'accessToken':appToken!, 'Content-Type':'application/json'});
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+
+    } catch(e) {
+      Exception(e);
+      return false;
+    }
+  }
+
+  Future<List<AllTattooList>> getAllTattoo() async {
+    final url = Uri.http('165.194.104.144:8888', '/api/scar/getAllTattoo');
+
+    try {
+      var appToken = await storage.read(key: 'appToken');
+
+      final response = await http.get(url, headers: {'accessToken':appToken!, 'Content-Type':'application/json'});
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> bodyMap = jsonDecode(response.body);
+        List<dynamic> dataMap = await bodyMap['data'];
+        List<AllTattooList> allTattooInfo =
+        dataMap.map((dynamic item) => AllTattooList.fromJson(item)).toList();
+
+        print("********test getAllTattoo request********");
+        print(allTattooInfo);
+
+        return allTattooInfo;
+      } else {
+        print("Failed to load AllTattoo");
+        throw Exception("Failed to load AllTattoo");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  Future<Tattoo> getTattooAllInfo() async {
+    final api = 'api/scar/' + scarId.toString() + '/getTattooAllInfo';
+
+    final url = Uri.http('165.194.104.144:8888', api);
+
+    try {
+      var appToken = await storage.read(key: 'appToken');
+
+      final response = await http.get(url, headers: {'accessToken':appToken!, 'Content-Type':'application/json'});
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> bodyMap = jsonDecode(response.body);
+        Map<String, dynamic> dataMap = await bodyMap['data'];
+
+        print("********test getTattooAllInfo request********");
+        print(dataMap);
+
+        return Tattoo.fromJson(dataMap);
+      } else {
+        print("Failed to load AllTattoo");
+        throw Exception("Failed to load AllTattoo");
+      }
+    } catch(e) {
+      rethrow;
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    if (checkIsFirstUser() == true) {
+      setState(() {
+        isFirst = true;
+      });
+    } else {
+      futureAllTattoo = getAllTattoo();
+      isFirst = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,21 +144,33 @@ class MyinfoState extends State<MyinfoScreen> {
               ),
             ),
           ),
-          GridView.builder(
-            physics: ScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemCount: 20,
-            itemBuilder: (context, index) =>
-                Container(
-                  width: 80,
-                  height: 80,
-                  margin: const EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xffEEF4FF),
-                    borderRadius: BorderRadius.circular(10.0)
+          isFirst ?
+          SizedBox(
+            height: 140,
+            child: Center(
+              child: Text("사진 촬영을 통해 나만의 타투를 만들어보세요! 🤹🏻", style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              )),
+            ),
+          ) : FutureBuilder<List<AllTattooList>>(
+            future: futureAllTattoo,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return buildGrid(snapshot);
+              } else if (snapshot.hasError) {
+                return SizedBox(
+                  height: 140,
+                  child: Center(
+                    child: Text("[ERROR] ${snapshot.error} !", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    )),
                   ),
-                ),
+                );
+              }
+              return CircularProgressIndicator();
+            },
           ),
         ],
       ),
@@ -111,4 +221,23 @@ class MyinfoState extends State<MyinfoScreen> {
       }
     }),
   );
+
+  Widget buildGrid(snapshot) {
+    return GridView.builder(
+      physics: ScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: 20,
+      itemBuilder: (context, index) =>
+          Container(
+            width: 80,
+            height: 80,
+            margin: const EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color: Color(0xffEEF4FF),
+                borderRadius: BorderRadius.circular(10.0)
+            ),
+          ),
+    );
+  }
 }
