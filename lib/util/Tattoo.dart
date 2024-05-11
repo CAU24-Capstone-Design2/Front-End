64b2c9ac862ecfafc9c936cc84c04e06aac9b6f9
Yoo Han <hanyoojin39@gmail.com>
2024-05-11 09:52:11 +0900

    feat: create Tattoo class for tattoo datas

---
+++ lib/util/Tattoo.dart
@@ -0,0 +1,20 @@
+class Tattoo {
+  final String scarImage;
+  final String segmentImage;
+  final String tattooImage;
+
+  const Tattoo({
+    required this.scarImage,
+    required this.segmentImage,
+    required this.tattooImage,
+  });
+
+  factory Tattoo.fromJson(Map<String, dynamic> json) {
+    return Tattoo(
+      scarImage: json['scarImage'],
+      segmentImage: json['segmentImage'],
+      tattooImage: json['tattooImage'],
+    );
+  }
+
+}
