f335ec8d2c4a36e4a60ddb55e1f8a952c1c8a2ca
Yoo Han <hanyoojin39@gmail.com>
2024-05-11 23:42:31 +0900

    feat: add download for each image in tattoos

--- lib/widget/widget_showTattoos.dart
+++ lib/widget/widget_showTattoos.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -22,6 +21,7 @@ class _ShowTattoosState extends State<ShowTattoos> with TickerProviderStateMixin
       await Permission.storage.request();
     } else {
       try {
+        print("image save test");
         GallerySaver.saveImage(url);
 
         print("이미지 저장 성공!!");
@@ -48,7 +48,7 @@ class _ShowTattoosState extends State<ShowTattoos> with TickerProviderStateMixin
                 ),
                 ElevatedButton(
                   onPressed: () {
-
+                    downloadImage('https://file.notion.so/f/f/0e18d6e9-2850-48da-b9ba-fcb851077aeb/c7e2fffd-1a95-4ec9-88f6-10e43affca80/scar1.png?id=ec57692d-bd3c-4d8e-8b1c-0fe85a88ce6b&table=block&spaceId=0e18d6e9-2850-48da-b9ba-fcb851077aeb&expirationTimestamp=1715529600000&signature=dUP06nw9wlV-fwmaGU4_gtLFVEkUnLblJgSZ5vjWGow&downloadName=scar1.png');
                   },
                   child: Text('Download'),
                 )
@@ -64,7 +64,7 @@ class _ShowTattoosState extends State<ShowTattoos> with TickerProviderStateMixin
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    downloadImage('https://file.notion.so/f/f/0e18d6e9-2850-48da-b9ba-fcb851077aeb/c7e2fffd-1a95-4ec9-88f6-10e43affca80/scar1.png?id=ec57692d-bd3c-4d8e-8b1c-0fe85a88ce6b&table=block&spaceId=0e18d6e9-2850-48da-b9ba-fcb851077aeb&expirationTimestamp=1715529600000&signature=dUP06nw9wlV-fwmaGU4_gtLFVEkUnLblJgSZ5vjWGow&downloadName=scar1.png');
+                    downloadImage('https://file.notion.so/f/f/0e18d6e9-2850-48da-b9ba-fcb851077aeb/236518d7-53bb-475f-a8c2-3611fbeea68b/nobg_masked_scar.png?id=749ddb46-31e4-4a1b-a25b-6dd370f9abd1&table=block&spaceId=0e18d6e9-2850-48da-b9ba-fcb851077aeb&expirationTimestamp=1715529600000&signature=7U0k5KChfp7WJaQClIMXTRLRjvKiPrvSaCnpSZDelIY&downloadName=nobg_masked_scar.png');
                   },
                   child: Text('Download'),
                 )
@@ -80,7 +80,7 @@ class _ShowTattoosState extends State<ShowTattoos> with TickerProviderStateMixin
                 ),
                 ElevatedButton(
                   onPressed: () {
-
+                    downloadImage('https://file.notion.so/f/f/0e18d6e9-2850-48da-b9ba-fcb851077aeb/85f52349-0674-4b68-8c54-96aa9fa5aa3a/nobg_masked_airplane.png?id=212dadee-082d-47c2-8e66-72b3188a0f93&table=block&spaceId=0e18d6e9-2850-48da-b9ba-fcb851077aeb&expirationTimestamp=1715529600000&signature=1uaNCrmvf9iGi-xVNmrC0FQmmNUlfn3ohGuEq7t_Iao&downloadName=nobg_masked_airplane.png');
                   },
                   child: Text('Download'),
                 )
